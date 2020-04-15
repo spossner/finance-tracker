12e86c7fd0b72c39aef14d2e5ae253da4f41b695
Sebastian Possner <sebastian.possner@brands4friends.de>
2020-04-15 09:10:01 +0200

    destroy stocks which are tracked by user

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -22,4 +22,12 @@ class UserStocksController < ApplicationController
       redirect_to my_portfolio_path
     end
   end
+
+  def destroy
+    stock = Stock.find(params[:id])
+    user_stock = UserStock.where(user_id: current_user.id, stock_id: stock.id).first
+    user_stock.destroy
+    flash[:notice] = "Successfully removed #{stock.name} (#{stock.ticker}) from your portfolio"
+    redirect_to my_portfolio_path
+  end
 end
